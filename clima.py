8fe468e262091cb0047e7551fe6f07e5235f4835
Ridi <118378677+RidiConHuracanes@users.noreply.github.com>
2024-05-09 20:28:47 -0600

    Create clima.py
    a

---
+++ clima.py
@@ -0,0 +1,8 @@
+import requests
+
+def consultar_clima(ciudad, api_key):
+  url = f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric'
+  print(url)response = requests.get(url)
+  data = response.json()
+  
+  if response.status_code == 200:
